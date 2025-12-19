006c0411caf84e738714549d4149c52526bbef05
OmkarPatil-14 <omkar1344patil@gmail.com>
2025-12-19 15:18:20 +0000

    updated-to-langchain-classic

--- main_rag_pipeline.py
+++ main_rag_pipeline.py
@@ -2,8 +2,8 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_ollama import OllamaLLM
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
 from langchain_core.language_models.llms import LLM
 from langchain_core.prompts import ChatPromptTemplate
